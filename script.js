93fe8ee6b66255c40ec6207af0e10be4fe602a6d
animesh-tyagi <animesh.tyagi.720@gmail.com>
2025-12-11 12:32:40 +0000

    added displayBooks function

--- script.js
+++ script.js
@@ -16,3 +16,45 @@ addBookToLibrary("J.K. Rowling", "Harry potter", 210, "read");
 addBookToLibrary("Marry Shelly", "Frankenstein", 130, "not read");
 addBookToLibrary("Suzanne Collins", "The Hunger Games", 320, "read");
 console.log(myLibrary);
+
+function displayBook(book) {
+    const display = document.querySelector(".display");    //select display div from html
+    const fragment = document.createDocumentFragment();
+
+    const card = document.createElement("div");
+    card.className = 'class';
+    card.dataset.id = book.id;
+
+    const author = document.createElement("p");
+    author.className = 'author';
+    author.innerHTML = `${book.author}`;
+    fragment.append(author);
+
+    const title = document.createElement("p");
+    title.className = 'title';
+    title.innerHTML = `${book.title}`;
+    fragment.append(title);
+
+    const pages = document.createElement("p");
+    pages.className = 'pages';
+    pages.innerHTML = `${book.pages}`;
+    fragment.append(pages);
+
+    const read = document.createElement("p");
+    read.className = 'read';
+    read.innerHTML = `${book.read}`;
+    fragment.append(read);
+
+    card.appendChild(fragment);
+    display.appendChild(card);
+
+
+}
+
+function displayBooks() {
+    const display = document.querySelector(".display");
+    display.innerHTML = '';                                //clear any elements from before
+    myLibrary.forEach(displayBook)
+}
+
+displayBooks();
